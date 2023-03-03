26de1fbf8267d97ed96582fd8098d369e65b8690
Dhanu <dhanushan.info@gmail.com>
2023-03-03 08:55:09 +0530

    Skills component designed.

--- src/container/Skills/SkillItem.jsx
+++ src/container/Skills/SkillItem.jsx
@@ -4,7 +4,20 @@ import React from 'react'
 import './Skills.scss';
 import { motion } from 'framer-motion';
 
+import html from '../../images/html.png'
+import css from '../../images/css.png'
+import javascript from '../../images/javascript.png'
 import react from '../../images/react.png'
+import git from '../../images/git.png'
+import node from '../../images/node.png'
+import sass from '../../images/sass.png'
+import python from '../../images/python.png'
+import rest from '../../images/api.png'
+import github from '../../images/GitHub.png'
+
+
+
+
 
 
 const scaleVariants = {
@@ -32,9 +45,9 @@ function SkillItem() {
               className="app__skills-item app__flex" key="html">
               
               <div className="app__flex skill__badge">
-                    <img src={react} alt="react" className="" />
+                    <img src={html} alt="html" className="" />
               </div>
-              <p className="p-text">React</p>
+              <p className="p-text">Html</p>
               </motion.div>
 
               <motion.div
@@ -44,9 +57,9 @@ function SkillItem() {
               className="app__skills-item app__flex" key="css">
               
               <div className="app__flex skill__badge">
-                    <img src={react} alt="react" className="" />
+                    <img src={css} alt="css" className="" />
               </div>
-              <p className="p-text">React</p>
+              <p className="p-text">CSS</p>
               </motion.div>
 
                    <motion.div
@@ -56,9 +69,9 @@ function SkillItem() {
               className="app__skills-item app__flex" key="javascript">
               
               <div className="app__flex skill__badge">
-                    <img src={react} alt="react" className="" />
+                    <img src={javascript} alt="JavaScript" className="" />
               </div>
-              <p className="p-text">React</p>
+              <p className="p-text">JavaScript</p>
               </motion.div>
 
                    <motion.div
@@ -70,7 +83,7 @@ function SkillItem() {
               <div className="app__flex skill__badge">
                     <img src={react} alt="react" className="" />
               </div>
-              <p className="p-text">React</p>
+              <p className="p-text">React js</p>
               </motion.div>
 
                    <motion.div
@@ -80,9 +93,9 @@ function SkillItem() {
               className="app__skills-item app__flex" key="node">
               
               <div className="app__flex skill__badge">
-                    <img src={react} alt="react" className="" />
+                    <img src={node} alt="node" className="" />
               </div>
-              <p className="p-text">React</p>
+              <p className="p-text">Node js</p>
               </motion.div>
 
                    <motion.div
@@ -92,9 +105,9 @@ function SkillItem() {
               className="app__skills-item app__flex" key="python">
               
               <div className="app__flex skill__badge">
-                    <img src={react} alt="react" className="" />
+                    <img src={python} alt="python" className="" />
               </div>
-              <p className="p-text">React</p>
+              <p className="p-text">Python</p>
               </motion.div>
 
                    <motion.div
@@ -104,9 +117,9 @@ function SkillItem() {
               className="app__skills-item app__flex" key="rest_api">
               
               <div className="app__flex skill__badge">
-                    <img src={react} alt="react" className="" />
+                    <img src={rest} alt="rest_api" className="" />
               </div>
-              <p className="p-text">React</p>
+              <p className="p-text">REST API</p>
               </motion.div>
 
                    <motion.div
@@ -116,9 +129,9 @@ function SkillItem() {
               className="app__skills-item app__flex" key="sass">
               
               <div className="app__flex skill__badge">
-                    <img src={react} alt="react" className="" />
+                    <img src={sass} alt="sass" className="" />
               </div>
-              <p className="p-text">React</p>
+              <p className="p-text">Sass</p>
               </motion.div>
 
                    <motion.div
@@ -128,9 +141,9 @@ function SkillItem() {
               className="app__skills-item app__flex" key="git">
               
               <div className="app__flex skill__badge">
-                    <img src={react} alt="react" className="" />
+                    <img src={git} alt="git" className="" />
               </div>
-              <p className="p-text">React</p>
+              <p className="p-text">Git</p>
               </motion.div>
 
                    <motion.div
@@ -140,9 +153,9 @@ function SkillItem() {
               className="app__skills-item app__flex" key="github">
               
               <div className="app__flex skill__badge">
-                    <img src={react} alt="react" className="" />
+                    <img src={github} alt="github" className="" />
               </div>
-              <p className="p-text">React</p>
+              <p className="p-text">Github</p>
               </motion.div>             
           
           </motion.div>
