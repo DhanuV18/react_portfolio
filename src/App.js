b37c50821bab11c279d2807ac7dfef069c24e22e
Dhanu <dhanushan.info@gmail.com>
2023-03-03 07:44:34 +0530

    About component completed

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
     <div className='app'>
       <Navbar />
       <Header />
-      {/* <About />  */}
+      <About /> 
       {/* <Skills /> 
       <Work />
       <Footer />       */}
