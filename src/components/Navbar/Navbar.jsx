7935a45f8aa6c7ab629f86ec743fbd6204e6faba
Dhanu <dhanushan.info@gmail.com>
2023-03-17 22:14:21 +0530

    bug fixed.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,9 @@
-import React, { useState } from 'react'
-import './Navbar.scss';
-import logo from '../../images/Dhanushan__logo.png'
-import { HiMenuAlt4 } from 'react-icons/hi'
-import { HiX } from 'react-icons/hi';
-import { motion } from 'framer-motion'
+import React, { useState } from "react";
+import "./Navbar.scss";
+import logo from "../../images/Dhanushan__logo.png";
+import { HiMenuAlt4 } from "react-icons/hi";
+import { HiX } from "react-icons/hi";
+import { motion } from "framer-motion";
 
 const Navbar = () => {
   const [toggle, setToggle] = useState(false);
@@ -11,10 +11,10 @@ const Navbar = () => {
   return (
     <nav className="app__navbar">
       <div className="app__navbar-logo">
-        <img src={logo} alt="logo" href="#" className='name__logo' />
+        <img src={logo} alt="logo" href="#" className="name__logo" />
       </div>
       <ul className="app__navbar-links">
-        {['home', 'about', 'skills', 'projects' , 'contact'].map((item) => (
+        {["home", "about", "skills", "projects", "contact"].map((item) => (
           <li className="app__flex p-text" key={`link-${item}`}>
             <div />
             <a href={`#${item}`}>{item}</a>
@@ -28,17 +28,19 @@ const Navbar = () => {
         {toggle && (
           <motion.div
             whileInView={{ x: [300, 0] }}
-            transition={{ duration: 0.5, ease: 'easeIn' }}
+            transition={{ duration: 0.5, ease: "easeIn" }}
           >
             <HiX onClick={() => setToggle(false)} />
             <ul>
-              {['home', 'about', 'skills', 'projects', 'contact'].map((item) => (
-                <li key={item}>
-                  <a href={`#${item}`} onClick={() => setToggle(false)}>
-                    {item}
-                  </a>
-                </li>
-              ))}
+              {["home", "about", "skills", "projects", "contact"].map(
+                (item) => (
+                  <li key={item}>
+                    <a href={`#${item}`} onClick={() => setToggle(false)}>
+                      {item}
+                    </a>
+                  </li>
+                )
+              )}
             </ul>
           </motion.div>
         )}
@@ -47,4 +49,4 @@ const Navbar = () => {
   );
 };
 
-export default Navbar
+export default Navbar;
